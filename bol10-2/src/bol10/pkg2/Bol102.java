3f27207308147462fcee526140de46378484d59d
ipampillonvieitez <ipampillonvieitez@danielcastelao.org>
2019-11-26 09:01:34 +0100

    first commit

---
+++ bol10-2/src/bol10/pkg2/Bol102.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol10.pkg2;
+
+/**
+ *
+ * @author ipampillonvieitez
+ */
+public class Bol102 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          Juego inicio = new Juego();
+        inicio.Operaciones();
+    
+    }
+    
+}
